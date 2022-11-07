c7cadf87ac95f99081c2279fa401bf313f726408
Your Name <you@example.com>
2022-11-07 14:36:13 +0100

    Attempt #1 at creating MP4 to MP3 converter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import './App.css';
 import sound from './audio/Minecraft_Hit.mp3';
-import Move from './Move'
-import KeyStroke from './KeyStroke'
+import Move from './Move';
+import KeyStroke from './KeyStroke';
+// var ffmpeg = require('ffmpeg');
+// import ToMp3 from './ToMp3';
 
 function App() {
   function play() {
     new Audio(sound).play();
+      setTimeout(() => {
+        new Audio(sound).play();
+      }, 1000);
+  }
+  function convertVideo() {
+    // let url = document.getElementById('url').value;
+    // if (url != '') {
+try {
+    //var process = new ffmpeg('./audio/testvideo.mp4');
+    process.then(function (video) {
+      //Callback mode
+      video.fnExtractSoundToMP3('./audio/test.mp3', function (error, file) {
+        if(!error) {
+          console.log('Audio file: ' + file);
+        } 
+      })
+    }, function (error) {
+      console.log('Error: ' + error);
+    });
+    console.log('placeholder');
+  } catch(err) {
+    console.log(err.code);
+    console.log(err.msg);
+  }
+  //}
   }
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={play}>playsound</button>
-        <KeyStroke/>
+        <form>
+          <input type='text' id="url" placeholder="url to yt video"></input>
+          <button type='submit' onClick={convertVideo} disabled>Convert!</button>
+        </form>
+        <KeyStroke />
         <Move />    
       </header>
     </div>
---
+++ src/ToMp3.jsx
@@ -0,0 +1,21 @@
+//const ffmpeg = require('ffmpeg');
+// function convert(url) {
+
+    // var process = new ffmpeg({url});
+    // process.then(function (video) {
+    //     // Callback mode
+    //     video.fnExtractSoundToMP3(url, function (error, file) {
+    //         if (!error)
+    //             console.log('Audio file: ' + file);
+    //     });
+    // }, function (err) {
+    //     console.log('Error: ' + err);
+    // });
+// }
+
+export default function ToMp3()
+{
+    return <form>
+        <input type="text"></input>
+    </form>
+}
